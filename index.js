a3a3ae5b5e39b2b9899d9e722345a2b0f9059e7e
Josh Bain <joshuambain@gmail.com>
2024-07-14 11:19:29 -0400

    complete javeascript events acting on events lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+// moveDodgerLeft function provided by lesson
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    //add condition for arrowRight
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+  });
+  // moveDodgerRight function created by Josh
+  function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(rightNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left +1}px`;
+    }
+  }
